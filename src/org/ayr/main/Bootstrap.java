a4185d7236e1ff8653088362abad5dd849b62f2a
ayrzjy <ayrzjy@gmail.com>
2015-06-25 15:19:16 +0800

    Create Empty Project

---
+++ src/org/ayr/main/Bootstrap.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.ayr.main;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Bootstrap {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
