2e168707007ce8aa931cd7fdb0976fe7e5a7fc30
ayrzjy <ayrzjy@gmail.com>
2015-06-25 15:54:33 +0800

    version 3

--- src/org/ayr/main/Bootstrap.java
+++ src/org/ayr/main/Bootstrap.java
@@ -13,7 +13,6 @@ public class Bootstrap {
      * @param args
      */
     public static void main(String[] args) {
-	System.out.println("Hello remote");
 	System.out.println("Hello Git");
     }
 }
