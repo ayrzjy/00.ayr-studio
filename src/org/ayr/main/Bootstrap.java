6802cb5c21b8dbc21ef5f853b92f5e4ec819fc9b
ayrzjy <ayrzjy@gmail.com>
2015-06-25 15:54:31 +0800

    modified by remote2

--- src/org/ayr/main/Bootstrap.java
+++ src/org/ayr/main/Bootstrap.java
@@ -14,6 +14,8 @@ public class Bootstrap {
      */
     public static void main(String[] args) {
 	System.out.println("Hello remote");
-	System.out.println("Hello Git");
+	for (int i = 0; i < 100; i++) {
+	    System.out.println("Hello Git");
+	}
     }
 }
