def1a2abbd5bce713e96128c0c508ed437214953
ayrzjy <ayrzjy@gmail.com>
2015-06-27 12:56:28 +0800

    201506271255

---
+++ src/org/ayr/iplatform/util/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2015/6/27.
+ */
+package org.ayr.iplatform.util;
--- src/org/ayr/main/Bootstrap.java
+++ src/org/ayr/main/Bootstrap.java
@@ -1,11 +1,10 @@
 /**
- * 
+ *
  */
 package org.ayr.main;
 
 /**
  * @author ayrzjy
- *
  */
 public class Bootstrap {
 
@@ -13,11 +12,12 @@ public class Bootstrap {
      * @param args
      */
     public static void main(String[] args) {
-	System.out.println("Hello remote");
-	for (int i = 0; i < 100; i++) {
-	    System.out.println("Hello Git");
-	}
-	System.out.println("Hello Git");
-	
+        System.out.println("Hello remote");
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hello Git");
+        }
+
+        System.out.println("Hello Git");
     }
 }
